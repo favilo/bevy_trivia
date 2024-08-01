ed2ea31dfdcc2666e9daf6613d59140b5b9a5d35
favilo <kevin.oberlies@elastic.co>
2024-08-01 11:03:18 -0700

    fix: Clippy warnings
    - I was using `let _ = ...` in a few places, which is a clippy warning.
    - I had an unnecessary `&` before `AsRef::as_ref``

--- src/menu/utils.rs
+++ src/menu/utils.rs
@@ -3,7 +3,7 @@ use bevy_quill::{Callback, Cx};
 
 pub fn open_link(cx: &mut Cx, link: impl AsRef<str> + Send + Sync + 'static) -> Callback {
     cx.create_callback(move || {
-        if let Err(error) = webbrowser::open(&link.as_ref()) {
+        if let Err(error) = webbrowser::open(link.as_ref()) {
             warn!("Failed to open link {error:?}");
         }
     })
